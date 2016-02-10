5644f5c8289947e69bad794fff4136b0a2b8ae4d
Andrija Karadžić <andrijak91@gmail.com>
2016-02-10 13:20:35 +0100

    commit-10-feb-2016
    PayPal integration

--- app/src/main/java/com/andro/enigma/activity/GetPackageActivity.java
+++ app/src/main/java/com/andro/enigma/activity/GetPackageActivity.java
@@ -1,6 +1,8 @@
 package com.andro.enigma.activity;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
@@ -8,17 +10,27 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.andro.enigma.R;
 import com.andro.enigma.adapter.ListAdapter;
 import com.andro.enigma.database.*;
 import com.andro.enigma.database.Package;
+import com.andro.enigma.helper.Helper;
 import com.andro.enigma.parser.JsonParser;
+import com.paypal.android.sdk.payments.PayPalAuthorization;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class GetPackageActivity extends Activity {
@@ -29,6 +41,15 @@ public class GetPackageActivity extends Activity {
     private String lang;
     CrosswordDbHelper mDbHelper;
     Button button;
+    PayPalPayment thingToBuy;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(Helper.CONFIG_ENVIRONMENT)
+            .clientId(Helper.CONFIG_CLIENT_ID)
+            .merchantName("Enigma Store")
+            .merchantPrivacyPolicyUri(
+                    Uri.parse("https://www.example.com/privacy"))
+            .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,11 +74,83 @@ public class GetPackageActivity extends Activity {
 
             @Override
             public void onClick(View v) {
-                new JSONParse().execute();
+                thingToBuy = new PayPalPayment(new BigDecimal("5"), "USD",
+                        "Enigma package: " +  packageTitle , PayPalPayment.PAYMENT_INTENT_SALE);
+
+                Intent intent = new Intent(GetPackageActivity.this, PaymentActivity.class);
+
+                intent.putExtra(PaymentActivity.EXTRA_PAYMENT, thingToBuy);
+                intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+                startActivityForResult(intent, Helper.REQUEST_CODE_PAYMENT);
+
+                //new JSONParse().execute();
             }
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Helper.REQUEST_CODE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                PaymentConfirmation confirm = data
+                        .getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirm != null) {
+                    try {
+                        System.out.println(confirm.toJSONObject().toString(4));
+                        System.out.println(confirm.getPayment().toJSONObject()
+                                .toString(4));
+                        Toast.makeText(getApplicationContext(), "Order placed",
+                                Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                System.out.println("The user canceled.");
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                System.out
+                        .println("An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+            }
+        } else if (requestCode == Helper.REQUEST_CODE_FUTURE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                PayPalAuthorization auth = data
+                        .getParcelableExtra(PayPalFuturePaymentActivity.EXTRA_RESULT_AUTHORIZATION);
+                if (auth != null) {
+                    try {
+                        Log.i("FuturePaymentExample", auth.toJSONObject()
+                                .toString(4));
+                        String authorization_code = auth.getAuthorizationCode();
+                        Log.i("FuturePaymentExample", authorization_code);
+                        sendAuthorizationToServer(auth);
+                        Toast.makeText(getApplicationContext(),
+                                "Future Payment code received from PayPal",
+                                Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        Log.e("FuturePaymentExample",
+                                "an extremely unlikely failure occurred: ", e);
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.i("FuturePaymentExample", "The user canceled.");
+            } else if (resultCode == PayPalFuturePaymentActivity.RESULT_EXTRAS_INVALID) {
+                Log.i("FuturePaymentExample",
+                        "Probably the attempt to previously start the PayPalService had an invalid PayPalConfiguration. Please see the docs.");
+            }
+        }
+    }
+
+    private void sendAuthorizationToServer(PayPalAuthorization authorization) {
+
+    }
+
+    @Override
+    public void onDestroy() {
+        // Stop service when done
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+
 
     private class JSONParse extends AsyncTask<String, String, JSONObject> {
         @Override
@@ -74,7 +167,7 @@ public class GetPackageActivity extends Activity {
         protected JSONObject doInBackground(String... args) {
             JsonParser jsonParser = new JsonParser();
 
-            JSONObject json = jsonParser.getJSONFromUrl("http://www.develop.enigma-aeiou.net/service/getenigmasforpackage?seckey=EnIgmAAEIOU&id=" + packageId + "&user=13");
+            JSONObject json = jsonParser.getJSONFromUrl(Helper.HOME_URL + "/service/getenigmasforpackage?seckey=EnIgmAAEIOU&id=" + packageId + "&user=13");
 
             return json;
         }
--- app/src/main/java/com/andro/enigma/helper/Helper.java
+++ app/src/main/java/com/andro/enigma/helper/Helper.java
@@ -1,7 +1,10 @@
 package com.andro.enigma.helper;
 
 import android.content.Context;
+import android.net.Uri;
 import android.preference.PreferenceManager;
+
+import com.paypal.android.sdk.payments.PayPalConfiguration;
 
 import java.util.HashMap;
 
@@ -9,6 +12,25 @@ import java.util.HashMap;
  * Created by Internet on 28-Jan-16.
  */
 public class Helper {
+
+    public static final String HOME_URL = "http://www.develop.enigma-aeiou.net";
+
+    //PayPal configuration
+
+    public static final String CONFIG_CLIENT_ID = "AT99fIs7li4ccLVrKjPCRxy3AJrbIi8jIZK9aDSCJb8pGwWPKsVbFEDnUkPDtUbsW6hZb5Rlz6T92g34";
+    public static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_SANDBOX;
+    public static final int REQUEST_CODE_PAYMENT = 1;
+    public static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+        .environment(CONFIG_ENVIRONMENT)
+        .clientId(CONFIG_CLIENT_ID)
+        .merchantName("Enigma Store")
+        .merchantPrivacyPolicyUri(
+                Uri.parse("https://www.example.com/privacy"))
+                .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"));
+
+
 
     public  HashMap<String, String> charMap;
 
